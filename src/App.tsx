3b5842cfc0d460737ee335df035c30768d1bf626
Chaitra-Kulkarni <rkchaitra22@gmail.com>
2024-07-29 13:54:41 +0530

    feat: initial page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 function App() {
-  return <h1>Hello JS!</h1>;
+  return (
+    <div className="border-2 border-fuchsia-800 h-screen flex flex-col items-center justify-center">
+      <div className="text-center">
+        <h1>Hello JS fellows!</h1>
+        <p>Below are the topics in JS that I've practiced!</p>
+      </div>
+      <div className="flex flex-row">
+        <div className="h-[10px] w-[10px] bg-cyan-500 mr-2"></div>
+        <div className="h-[10px] w-[10px] bg-orange-500"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
